26f6e8c1ce4f39c873b607161b4b2b317c4621f7
pachgadehardik <pachgadehardik@gmail.com>
2020-10-22 20:40:05 +0530

    Added Exception Class

---
+++ src/main/java/com/capg/cabinvoicegenerator/Exceptions/CabInvoiceException.java
@@ -0,0 +1,20 @@
+package com.capg.cabinvoicegenerator.Exceptions;
+
+public class CabInvoiceException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public enum ExceptionType{
+		NULLRECORD, OTHERTYPE
+	}
+	
+	private ExceptionType exceptionType;
+	private String msg;
+	public CabInvoiceException(ExceptionType exceptionType,String msg) {
+		super(msg);
+		this.exceptionType = exceptionType;
+	}
+	
+}
